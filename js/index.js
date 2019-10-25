0d784260dc61d0e171e072bb761185adcb7d6d3a
Heather Nuffer <zzzuzz@gmail.com>
2019-10-25 15:18:42 -0600

    Add button that changes content to new images and jokes

--- js/index.js
+++ js/index.js
@@ -37,6 +37,47 @@ const siteContent = {
   },
 };
 
+const siteContent2 = {
+  "nav": {
+    "nav-item-1": "Here",
+    "nav-item-2": "Are",
+    "nav-item-3": "Some",
+    "nav-item-4": "Jokes",
+    "nav-item-5": "For",
+    "nav-item-6": "You",
+    "nav-item-7": "to",
+    "nav-item-8": "Enjoy!",
+    "img-src": "https://upload.wikimedia.org/wikipedia/commons/6/61/Joke_Alert.png"
+  },
+  "cta": {
+    "h1": "Make Someone Smile ",
+    "button": "Get Started",
+    "img-src": "https://upload.wikimedia.org/wikipedia/commons/d/d5/Funny_face_-_panoramio.jpg"
+  },
+  "main-content": {
+    "features-h4":"Variation of a Classic",
+    "features-content": "Why did half of a chicken cross the road? To get to his other side!",
+    "about-h4":"From a Fun Guy",
+    "about-content": "How much room is need for fungi to grow? As mushroom as possible.",
+    "middle-img-src": "img/road.jpg",
+    "services-h4":"Nice, Nice Kitty",
+    "services-content": "What side of a cat has the most fur? The outside.",
+    "product-h4":"It's the Thought that Counts",
+    "product-content": "What did the bald man say when he got a comb for Christmas? Thanks, I never part with it.",
+    "vision-h4":"Furnace Substitute",
+    "vision-content": "If you ever get cold, why should you stand in the corner of a room for a while? They're usually 90 degrees.",
+  },
+  "contact": {
+    "contact-h4" : "Contact",
+    "address" : "Lindon, UT, USA",
+    "phone" : "1 (888) 888-8888",
+    "email" : "zzzuzz@gmail.com",
+  },
+  "footer": {
+    "copyright" : "Copyright Heather Nuffer 2019"
+  },
+};
+
 //Selectors for global elements
 let body = document.getElementsByTagName("body")[0];
 let h4s = document.getElementsByTagName("h4");
@@ -58,19 +99,19 @@ let navAs = document.querySelectorAll("nav a");
 let navAsText = Object.values(siteContent["nav"]);
 for (let i=0; i<navAs.length; i++) {
   navAs[i].textContent = navAsText[i];
-  navAs[i].style.color = "green";
+  navAs[i].style.color = "#02C927"; //green
 }
 
 //Utilize .appendChild() and .prepend() to add two new 
 //items to the navigation system. You can call them whatever you want.
 const newNavItem1 = document.createElement("a");
 newNavItem1.textContent = "Log In";
-newNavItem1.style.color = "green";
+newNavItem1.style.color = "#02C927"; //green
 nav.appendChild(newNavItem1);
 
 const newNavItem2 = document.createElement("a");
 newNavItem2.textContent = "Store";
-newNavItem2.style.color = "green";
+newNavItem2.style.color = "#02C927"; //green
 nav.prepend(newNavItem2);
 
 
@@ -84,6 +125,7 @@ ctaButton.textContent = siteContent["cta"]["button"];
 let mainSection = document.querySelector(".main-content");
 
 //top-content
+let topSection = document.querySelector(".top-content");
 let featuresH4 = document.querySelector(".top-content h4");
 featuresH4.textContent = siteContent["main-content"]["features-h4"];
 let featuresP = document.querySelector(".text-content p");
@@ -192,4 +234,84 @@ styleButton.onclick = function() {
     dark = true;
   }
   
+}
+
+// ***Add button for user to click to change content***
+
+const contentButton = document.createElement("button");
+contentButton.textContent = "Change Content";
+contentButton.style.cssText = "background: white; \
+  border: 1px double black; \
+  font-size: 1rem; \
+  padding: 0.75rem 1rem; \
+  margin: 1.25rem auto; \
+  display: block;";
+  
+footer.prepend(contentButton);
+contentButton.onmouseover = function() {
+  this.style.background = "#B0B0B0";
+  this.style.color = "white";
+}
+contentButton.onmouseout = function() {
+  this.style.background = "white";
+  this.style.color = "black";
+}
+
+//Event handler for contentButton click
+contentButton.onclick = function() {
+  //nav
+  logo.setAttribute('src', siteContent2["nav"]["img-src"]);
+  navAs = document.querySelectorAll("nav a");
+  navAsText = Object.values(siteContent2["nav"]);
+  for (let i=0; i<navAs.length; i++) { 
+    navAs[i].textContent = navAsText[i];
+  }
+
+  //cta
+  ctaH1.textContent = siteContent2["cta"]["h1"];
+  let smile = document.createElement("img");
+  smile.src= "https://upload.wikimedia.org/wikipedia/commons/c/c7/Smile2.svg";
+  ctaH1.append(smile);
+  ctaImg.setAttribute('src', siteContent2["cta"]["img-src"]);
+  ctaImg.style.maxWidth = "40%";
+  ctaImg.style.borderRadius = "5px";
+
+  //main-content
+  featuresH4.textContent = siteContent2["main-content"]["features-h4"];
+  featuresP.textContent = siteContent2["main-content"]["features-content"];
+  aboutH4.textContent = siteContent2["main-content"]["about-h4"];
+  aboutP.textContent = siteContent2["main-content"]["about-content"];
+  midImg.setAttribute('src', siteContent2["main-content"]["middle-img-src"]);
+  servicesH4.textContent = siteContent2["main-content"]["services-h4"];
+  servicesP.textContent = siteContent2["main-content"]["services-content"];
+  productH4.textContent = siteContent2["main-content"]["product-h4"];
+  productP.textContent = siteContent2["main-content"]["product-content"];
+  visionH4.textContent = siteContent2["main-content"]["vision-h4"];
+  visionP.textContent = siteContent2["main-content"]["vision-content"];
+
+  //Add a joke to top-content
+  let newJoke = document.createElement("div");
+  newJoke.classList.add("text-content");
+  let newJokeH4 = document.createElement("h4");
+  newJokeH4.textContent = "Scent Search";
+  let newJokeP = document.createElement("p");
+  newJokeP.textContent = "Why did the skunk cross the road? To get to the odor side.";
+  newJoke.appendChild(newJokeH4);
+  newJoke.appendChild(newJokeP);
+  topSection.append(newJoke);
+
+  //Adjust spacing for text-content
+  let textContentDivs = document.querySelector(".text-content");
+  for (let i=0; i<textContentDivs.length; i++) {
+    textContentDivs[i].style.margin = "0.5rem 2rem";
+  }
+
+  //contact
+  contactAddress.textContent = siteContent2["contact"]["address"];
+  contactEmail.textContent = siteContent2["contact"]["email"];
+
+  //footer
+  footerCopyright.textContent = siteContent2["footer"]["copyright"];
+  
+
 }
